5678b0156c569673886d5ac62c4770a6b1fa0af8
Slipp Douglas Thompson <git@capnslipp.com>
2016-09-09 19:36:24 -0500

    Unified both operators to `??=` & renamed to `NilCoalescingAssignmentOperator`
    * Once again, decided that a rename for this extension is appropriate: `OptionallyAssignOperator.swift` → `NilCoalescingAssignmentOperator.swift`
    * Renamed `=??` operator to `??=`.  
    	‣ I realized that we either have an optional for the `lhs` or the `rhs`, so both can be unified under one operator and the compiler will decide which specialization to use.  _Ultimately, both are just saying “assign to the LHS the first thing that's non-`nil`”._
    * Added variant that takes an optional for both the `lhs` & `rhs`.  
    	‣ If both sides are non-nil, prefers the existing `lhs` value.  
    	‣ It's quite plausible that this could happen regularly.
    * Updated header doc-comment accordingly.

--- NilCoalescingAssignmentOperator.swift
+++ NilCoalescingAssignmentOperator.swift
@@ -10,23 +10,19 @@
 /// 	    `_someIvar = someArg ?? _someIvar`  
 /// 	But each of those are a bit messy for a simple maybe-assignment.
 /// @usage:
-/// 	1. Add `OptionallyAssignOperator.swift` to your project.  
-/// 	2. Use like this: `_someIvar =?? someArg` (given `_someIvar:Int` & `someArg:Int?`).
+/// 	1. Add `NilCoalescingAssignmentOperator.swift` to your project.
+/// 	2. Use like this: `_someIvar ??= someArg` (given `_someIvar:Int`/`someArg:Int?`, or given `_someIvar:Int?`/`someArg:Int`).
 /// @interwebsouce: https://gist.github.com/capnslipp/9d27a8af34b6ad3402c1d5e5f2a47d0f
 
 
 
-infix operator =?? : AssignmentPrecedence
+infix operator ??= : AssignmentPrecedence
 
 /// Assigns only when `rhs` is non-`nil`.
 /// effectively: `lhs = rhs ?? lhs`
-public func =??<Wrapped>(lhs:inout Wrapped, rhs:Optional<Wrapped>) {
+public func ??=<Wrapped>(lhs:inout Wrapped, rhs:Optional<Wrapped>) {
 	if rhs != nil { lhs = rhs! }
 }
-
-
-
-infix operator ??= : AssignmentPrecedence
 
 /// Assigns only when `lhs` is `nil`.
 /// effectively: `lhs = lhs ?? rhs`
@@ -35,4 +31,13 @@ public func ??=<Wrapped>(lhs:inout Optional<Wrapped>, rhs:Wrapped) {
 }
 public func ??=<Wrapped>(lhs:inout ImplicitlyUnwrappedOptional<Wrapped>, rhs:Wrapped) {
 	if lhs == nil { lhs = rhs }
+}
+
+/// Assigns only when `lhs` is `nil` and `rhs` is non-`nil`
+/// effectively: `lhs = lhs ?? rhs ?? nil`
+public func ??=<Wrapped>(lhs:inout Optional<Wrapped>, rhs:Optional<Wrapped>) {
+	if lhs == nil && rhs != nil { lhs = rhs }
+}
+public func ??=<Wrapped>(lhs:inout ImplicitlyUnwrappedOptional<Wrapped>, rhs:Optional<Wrapped>) {
+	if lhs == nil && rhs != nil { lhs = rhs }
 }
