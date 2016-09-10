792dd1486f96781361390b46043d88d024e9e764
Slipp Douglas Thompson <git@capnslipp.com>
2016-09-09 19:21:45 -0500

    Revised to `??=` & `=??` variants
    I realized there's two use-cases here— assigning only when RHS is non-nil, and assigning only when the LHS is nil.
    
    * Renamed the existing `=?` operator to `=??`.
    * Added new `??=<Wrapped>(lhs:inout Optional<Wrapped>, rhs:Wrapped)` operator, which only does the assignment if the `lhs` is nil.  
    	‣ Works like Ruby's `||=` operator— _`a ??= b` means `a = a ?? b`.
    	* Also added a variant with takes a `lhs:inout ImplicitlyUnwrappedOptional<Wrapped>`.  
    		‣ Swift doesn't extend `Optional` usages to work with `ImplicitlyUnwrappedOptional`s too, I guess.
    * Updated header doc-comment for the renamed & new operator.

--- OptionallyAssignOperator.swift
+++ OptionallyAssignOperator.swift
@@ -1,22 +1,38 @@
 /// @creator: Slipp Douglas Thompson
 /// @license: WTFPL
 /// @purpose: A concise operator syntax for assigning to a non-`Optional` from an `Optional`.
-/// @why: Sometimes in Swift you want to assign only if the RHS is non-`nil`.  
-/// 	Say you have `someArg:Int?` and `_someIvar:Int`.  You could use a single-line `if`:  
-/// 	    `if someArg != nil { _someIvar = someArg! }`  
+/// @why:
+/// 	Sometimes in Swift you want to assign only if the RHS is non-`nil`.  
+/// 	Say you have `someArg:Int?` and `_someIvar:Int`.  You could use a single-line `if`:  
+/// 	    `if someArg != nil { _someIvar = someArg! }`  
 /// 	Or you could use a ternary or nil-coalesce:  
-/// 	    `_someIvar = someArg != nil ? someArg! : _someIvar`  
-/// 	    `_someIvar = someArg ?? _someIvar`  
+/// 	    `_someIvar = someArg != nil ? someArg! : _someIvar`  
+/// 	    `_someIvar = someArg ?? _someIvar`  
 /// 	But each of those are a bit messy for a simple maybe-assignment.
 /// @usage:
 /// 	1. Add `OptionallyAssignOperator.swift` to your project.  
-/// 	2. Use like this: `_someIvar ?= someArg`
+/// 	2. Use like this: `_someIvar =?? someArg` (given `_someIvar:Int` & `someArg:Int?`).
 /// @interwebsouce: https://gist.github.com/capnslipp/9d27a8af34b6ad3402c1d5e5f2a47d0f
 
 
 
-infix operator =? : AssignmentPrecedence
+infix operator =?? : AssignmentPrecedence
 
-func =?<Wrapped>(lhs:inout Wrapped, rhs:Optional<Wrapped>) {
+/// Assigns only when `rhs` is non-`nil`.
+/// effectively: `lhs = rhs ?? lhs`
+func =??<Wrapped>(lhs:inout Wrapped, rhs:Optional<Wrapped>) {
 	if rhs != nil { lhs = rhs! }
+}
+
+
+
+infix operator ??= : AssignmentPrecedence
+
+/// Assigns only when `lhs` is `nil`.
+/// effectively: `lhs = lhs ?? rhs`
+func ??=<Wrapped>(lhs:inout Optional<Wrapped>, rhs:Wrapped) {
+	if lhs == nil { lhs = rhs }
+}
+func ??=<Wrapped>(lhs:inout ImplicitlyUnwrappedOptional<Wrapped>, rhs:Wrapped) {
+	if lhs == nil { lhs = rhs }
 }
