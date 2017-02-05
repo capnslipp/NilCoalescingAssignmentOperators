ad2d11dd3ad1b48babc0bb28472e2d94f3843b55
Slipp Douglas Thompson <git@capnslipp.com>
2017-02-05 16:53:12 -0600

    Minor: Switch syntax from `Optional<Wrapped>` to `Wrapped?`
    * For consistency with Swift's built-in `??` operator function, using operator forms of optionals in args:  
    	⁃ `Optional<Wrapped>`→`Wrapped?`  
    	⁃ `ImplicitlyUnwrappedOptional<Wrapped>`→`Wrapped!`

--- NilCoalescingAssignmentOperator.swift
+++ NilCoalescingAssignmentOperator.swift
@@ -20,29 +20,29 @@ infix operator ??= : AssignmentPrecedence
 
 /// Assigns only when `rhs` is non-`nil`.
 /// effectively: `lhs = rhs ?? lhs`
-public func ??=<Wrapped>(lhs:inout Wrapped, rhsClosure:(@autoclosure ()throws->Optional<Wrapped>)) rethrows {
+public func ??=<Wrapped>(lhs:inout Wrapped, rhsClosure:(@autoclosure ()throws->Wrapped?)) rethrows {
 	let rhs = try rhsClosure()
 	if rhs != nil { lhs = rhs! }
 }
 
 /// Assigns only when `lhs` is `nil`.
 /// effectively: `lhs = lhs ?? rhs`
-public func ??=<Wrapped>(lhs:inout Optional<Wrapped>, rhsClosure:(@autoclosure ()throws->Wrapped)) rethrows {
+public func ??=<Wrapped>(lhs:inout Wrapped?, rhsClosure:(@autoclosure ()throws->Wrapped)) rethrows {
 	if lhs == nil { lhs = try rhsClosure() }
 }
-public func ??=<Wrapped>(lhs:inout ImplicitlyUnwrappedOptional<Wrapped>, rhsClosure:(@autoclosure ()throws->Wrapped)) rethrows {
+public func ??=<Wrapped>(lhs:inout Wrapped!, rhsClosure:(@autoclosure ()throws->Wrapped)) rethrows {
 	if lhs == nil { lhs = try rhsClosure() }
 }
 
 /// Assigns only when `lhs` is `nil` and `rhs` is non-`nil`
 /// effectively: `lhs = lhs ?? rhs ?? nil`
-public func ??=<Wrapped>(lhs:inout Optional<Wrapped>, rhsClosure:(@autoclosure ()throws->Optional<Wrapped>)) rethrows {
+public func ??=<Wrapped>(lhs:inout Wrapped?, rhsClosure:(@autoclosure ()throws->Wrapped?)) rethrows {
 	if lhs == nil {
 		let rhs = try rhsClosure()
 		if rhs != nil { lhs = rhs }
 	}
 }
-public func ??=<Wrapped>(lhs:inout ImplicitlyUnwrappedOptional<Wrapped>, rhsClosure:(@autoclosure ()throws->Optional<Wrapped>)) rethrows {
+public func ??=<Wrapped>(lhs:inout Wrapped!, rhsClosure:(@autoclosure ()throws->Wrapped?)) rethrows {
 	if lhs == nil {
 		let rhs = try rhsClosure()
 		if rhs != nil { lhs = rhs }
