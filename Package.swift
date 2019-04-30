a1c27feaf294a7284f8908706c474e4160f0903d
Slipp Douglas Thompson <git@capnslipp.com>
2019-04-29 19:20:20 -0500

    Updated for SPM
    * Added all the additional modern fields to `Package.swift`— `products:`, `dependencies:`, `targets:`, and `swiftLanguageVersions:`.  
    	▸ I added all the Swift versions that the code will compile under — 3, 4, 4.2, & 5 — as `.version("…")` enum entries (instead of using the built-in `.v4`, etc. entries) because the package spec doesn't otherwise allow specifying newer-than-current entries, which is necessary when shadowing this package file for older Swift versions.
    * Enabled `DEFINES_MODULE`/“Defines Module”.

--- Package.swift
+++ Package.swift
@@ -2,5 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "NilCoalescingAssignmentOperators"
+	name: "NilCoalescingAssignmentOperators",
+	products: [
+		.library(name: "NilCoalescingAssignmentOperators", targets: [ "NilCoalescingAssignmentOperators" ]),
+	],
+	dependencies: [],
+	targets: [
+		.target(name: "NilCoalescingAssignmentOperators", dependencies: [], path: "Sources/"),
+	],
+	swiftLanguageVersions: [
+		.version("3"),
+		.version("4"),
+		.version("4.2"),
+		.version("5"),
+	]
 )
